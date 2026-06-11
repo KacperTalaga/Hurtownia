2a24412df5bc503c1c163d9f710f1697a2ace1f9
Kinga Szeliga <kingaszeliga@MacBook-Air-Kinga.local>
2026-06-11 16:26:06 +0200

    dodaj opisy techniczne materiałów

--- models/towar.py
+++ models/towar.py
@@ -47,7 +47,12 @@ class MaterialPlytowy(Towar):
         self.szerokosc = szerokosc
 
     def opis_techniczny(self) -> str:
-        pass
+        """Zwraca opis techniczny materiału płytowego."""
+        return (
+            f"Materiał płytowy: grubość {self.grubosc}, "
+            f"długość {self.dlugosc}, szerokość {self.szerokosc}."
+        )
+        
 
 
 class MaterialSypki(Towar):
@@ -57,7 +62,16 @@ class MaterialSypki(Towar):
         self.workowany = workowany
 
     def opis_techniczny(self) -> str:
-        pass
+        """Zwraca opis techniczny materiału sypkiego."""
+        if self.workowany:
+            sposob_pakowania: str = "workowany"
+        else:
+            sposob_pakowania = "luzem"
+
+        return (
+            f"Materiał sypki: gęstość {self.gestosc}, "
+            f"sposób pakowania: {sposob_pakowania}."
+        )
 
 
 class MaterialDluzycowy(Towar):
@@ -67,4 +81,8 @@ class MaterialDluzycowy(Towar):
         self.przekroj = przekroj
 
     def opis_techniczny(self) -> str:
-        pass
+        """Zwraca opis techniczny materiału dłużycowego."""
+        return (
+            f"Materiał dłużycowy: długość {self.dlugosc}, "
+            f"przekrój {self.przekroj}."
+        )
